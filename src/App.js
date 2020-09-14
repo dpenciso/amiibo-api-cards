3d8799840d08d37ac806360d307f61876d27d475
David Enciso <dpenciso@msn.com>
2020-09-14 14:51:41 -0600

    put search back at top

--- src/App.js
+++ src/App.js
@@ -2,43 +2,25 @@ import React from 'react';
 import './App.css';
 import Amiibo from './amiibo';
 import Header from './header';
-import SearchBox from './search';
-import amiiboData from './data/amiibo.json'
+import Search from './search'
 
-const amiiboCharacters = amiiboData.amiibo;
-
-const characterName = amiiboCharacters.map(amiiboCharacter => {
-  return amiiboCharacter.name.toLowerCase();
-});
-
-// const eachName = characterName.map(charName => {
-//   return {charName}
-// })
 
 class App extends React.Component{
-  
-  constructor(){
-    super();
-    this.state = {
-      names: [{ characterName }],
-      // name: [{ eachName }],
-      characters: [{ amiiboCharacters }],
-      searchField: ''
+
+    render() {
+      
+      return (
+        <div className="App">
+          <Header/>
+          <Search placeholder='Enter Amiibo Name...'/>
+          <Amiibo />
+        </div>
+      )
     }
   }
-  render(){
-    const {names, searchField} = this.state
-    const filteredNames = names.filter(name =>(
-      name.characterName.includes(searchField.toLowerCase)
-    ))
-    return (
-      <div className="App">
-        <Header/>
-        <SearchBox placeholder='Enter Amiibo name...' handleChange={(e) => this.setState({searchField:e.target.value})}/>
-        <Amiibo names = {filteredNames}/>
-      </div>
-    )
-  }
-}
+  
+
+
+  
 
 export default App;
--- src/amiibo.js
+++ src/amiibo.js
@@ -1,47 +1,71 @@
 import React from 'react';
 import amiiboData from './data/amiibo.json'
 import './amiibo.css'
-
-const amiiboChars = amiiboData.amiibo;
-
+import SearchBox from './search';
 
 
-function Amiibo() {
-    return (
+class Amiibo extends React.Component{
+    
+    state = {
+        amiiboCharacters: amiiboData.amiibo,
+        name: amiiboData.amiibo.map(names => {
+          return names.name
+        }),
+        searchField: ''
+        }
+
+    render() {
         
-        <div className="mainDiv">
-            {
-                amiiboChars.map(amiiboChar => {
-                    let bkgrndImg = amiiboChar.image;
-                    return (
-                        <div key={amiiboChar.tail} className="amiibo" style = {{ backgroundImage: `url(${bkgrndImg})` }}>
-                            <div>
-                                <h1>
-                                    Name: {amiiboChar.name}
-                                </h1>
-                            </div>
-                            <div>
-                                <h2>
-                                    Game: {amiiboChar.gameSeries}
-                                </h2>
-                            </div>
-                            <div>
-                                <h2>
-                                    Series: {amiiboChar.amiiboSeries}
-                                </h2>
-                            </div>
-                            <div>
-                                <h2>
-                                    NA Release: {amiiboChar.release.na}
-                                </h2>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-};
+        
+        return(
+            <div className="mainDiv">
+                {/* <div>
+                    <SearchBox placeholder='Enter Amiibo name...' handleChange={(e) => this.setState({searchField:e.target.value})}/>
+                </div> */}
+                
+                    {
+                        this.state.amiiboCharacters.map(amiiboChar => {
+                            let bkgrndImg = amiiboChar.image;
+                            let charName = amiiboChar.name;
+                            let charGame = amiiboChar.gameSeries;
+                            let charSeries = amiiboChar.amiiboSeries;
+                            let charRelease = amiiboChar.release.na;
+                            // const {name, searchField} = this.state;
+                            // const filteredNames = name.filter(charName =>(
+                            // charName.includes(searchField)));
+                            return (
+                                <div key={amiiboChar.tail} className="amiibo" style = {{ backgroundImage: `url(${bkgrndImg})` }}>
+                                    <div>
+                                        <h1>
+                                            Name: {charName}
+                                        </h1>
+                                    </div>
+                                    <div>
+                                        <h2>
+                                            Game: {charGame}
+                                        </h2>
+                                    </div>
+                                    <div>
+                                        <h2>
+                                            Series: {charSeries}
+                                        </h2>
+                                    </div>
+                                    <div>
+                                        <h2>
+                                            NA Release: {charRelease}
+                                        </h2>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                
+            </div>
+        )
+    }
+}
+
+
 
 
 export default Amiibo;
